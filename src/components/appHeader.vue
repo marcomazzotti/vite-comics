6478ed2f52ef521609fa416ba7fdb49d72f6059e
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-04-13 22:37:45 +0200

    Add: dinamic menu (completed exercise)

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,6 +1,62 @@
 <script>
 export default {
-  name: "appHeader"
+  name: "appHeader",
+  data(){
+    return {
+      links: [
+        {
+          text: "CHARACTERS",
+          url: "#",
+          current: false
+        },
+        {
+          text: "COMICS",
+          url: "#",
+          current: true
+        },
+        {
+          text: "MOVIES",
+          url: "#",
+          current: false
+        },
+        {
+          text: "TV",
+          url: "#",
+          current: false
+        },
+        {
+          text: "GAMES",
+          url: "#",
+          current: false
+        },
+        {
+          text: "COLLECTIBLES",
+          url: "#",
+          current: false
+        },
+        {
+          text: "VIDEOS",
+          url: "#",
+          current: false
+        },
+        {
+          text: "FAN",
+          url: "#",
+          current: false
+        },
+        {
+          text: "NEWS",
+          url: "#",
+          current: false
+        },
+        {
+          text: "SHOP",
+          url: "#",
+          current: false
+        },
+      ]
+    }
+  }
 }
 </script>
 
@@ -12,16 +68,10 @@ export default {
       </div>
       <div class="menu">
         <ul class="menu-items">
-          <li>CHARACTERS</li>
-          <li>COMICS</li>
-          <li>MOVIES</li>
-          <li>TV</li>
-          <li>GAMES</li>
-          <li>COLLECTIBLES</li>
-          <li>VIDEOS</li>
-          <li>FANS</li>
-          <li>NEWS</li>
-          <li>SHOP</li>
+          <li v-for="link in links" :class="link.current ? 'active' : ''">
+            <a href="{{ link.url }}">{{ link.text }}</a>
+            <div v-show="link.current" class="active-bar"></div>
+          </li>
         </ul>
       </div>
     </div>
@@ -29,6 +79,8 @@ export default {
 </template>
 
 <style scoped lang="scss">
+@use "../style/partials/variables" as *;
+
   header{
     height: 90px;
     display: flex;
@@ -43,16 +95,41 @@ export default {
   }
 
   .logo{
-    width: 60px;
+    width: 20%;
+
+    img{
+      width: 60px;
+    }
   }
 
   .menu-items{
     display: flex;
     list-style-type: none;
     font-size: 0.8rem;
+    gap: 1.3rem;
 
-    li {
-      padding-left: 1rem;
+    li{
+      color: black;
+      position: relative;
+      line-height: 85px;
+      
+      &.active, &:hover{
+        color: $primary;
+        border-bottom: 5px solid $primary;
+      }
+
+      a {
+      text-decoration: none;
+      color: inherit;
+      }
+
+      .active-bar{
+        width: 100%;
+        height: 5px;
+        background-color: $primary;
+        position: absolute;
+        bottom: 90px;
+      }
     }
   }
 </style>
