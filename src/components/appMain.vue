c9452fc230c45dcf0137d20559f7d254d177b0cc
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-04-14 15:55:15 +0200

    Add: dynamic cards

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -1,23 +1,146 @@
 <script>
+import heroCards from './heroCards.vue';
 export default {
-  name: "appMain"
+  name: "appMain",
+  components:{
+    heroCards
+  },
+  data(){
+    return {
+      heroes: [
+        {
+          thumb: 'https://www.coverbrowser.com/image/action-comics/1-1.jpg',
+          price: '$19.99',
+          series: 'Action Comics',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg',
+          price: '$3.99',
+          series: 'American Vampire 1976',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg',
+          price: '$16.99',
+          series: 'Aquaman',
+          type: 'graphic novel',
+        },
+        {
+          thumb: 'https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg',
+          price: '$2.99',
+          series: 'Batgirl',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg',
+          price: '$3.99',
+          series: 'Batman',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg',
+          price: '$2.99',
+          series: 'Batman Beyond',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg',
+          price: '$3.99',
+          series: 'Batman/Superman',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg',
+          price: '$4.99',
+          series: 'Batman/Superman Annual',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg',
+          price: '$5.99',
+          series: 'Batman: The Joker War Zone',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg',
+          price: '$6.99',
+          series: 'Batman: Three Jokers',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg',
+          price: '$4.99',
+          series: 'Batman: White Knight Presents: Harley Quinn',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg',
+          price: '$16.99',
+          series: 'Catwoman',
+          type: 'graphic novel',
+        },
+      ]
+    }
+  }
 }
 </script>
 
 <template>
-  <div class="content">
-    <div class="container">
-      <h3>-- Content Goes Here --</h3>
+  <main>
+    <div class="content-top"></div>
+    <div class="content">
+      <div class="container">
+        <div class="current-series">
+          <h4>CURRENT SERIES</h4>
+        </div>
+        <div class="row">
+          <heroCards v-for="(heroe, index) in heroes" :key="index" :image="heroe.thumb" :title="heroe.series"/>
+        </div>
+        <div class="show-more"><h6>SHOW MORE</h6></div>
+      </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <style scoped lang="scss">
+@use "../style/partials/variables" as *;
+  .content-top{
+    height: 200px;
+    background-color: lightgreen;
+  }
   .content{
     color: white;
     background-color: black;
-    height: 90px;
     display: flex;
     align-items: center;
+    position: relative;
+    padding: 1rem 0;
+
+    .current-series{
+      background-color: $primary;
+      display: inline-block;
+      padding: 0.5rem 1rem;
+      position: absolute;
+      top: -15px;
+    }
+
+    .row{
+      display: flex;
+      align-items: stretch;
+      justify-content: center;
+      flex-wrap: wrap;
+      padding: 1rem 0 2rem 0;
+    }
+
+    .show-more{
+      background-color: $primary;
+      display: inline-block;
+      padding: 0.5rem 3rem;
+      position: absolute;
+      right: 50%;
+      bottom: 15px;
+      transform: translateX(50%);
+    }
   }
 </style>
---
+++ src/components/heroCards.vue
@@ -0,0 +1,31 @@
+<script>
+export default{
+  name: "heroCards",
+  props:{
+    image: String,
+    title: String
+  }
+}
+</script>
+
+<template>
+  <div class="card">
+    <img :src="image" alt="">
+    <h5>{{ title }}</h5>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .card{
+    width: calc(100% / 6 - 10px);
+    margin: 5px;
+    // margin-bottom: -30px;
+    // background-color: wheat;
+
+    img{
+      height: 70%;
+      object-fit: cover;
+      object-position: 100% 0%;
+    }
+  }
+</style>
